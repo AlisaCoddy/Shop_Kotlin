c5079f3925e2d07fd1dded561d22c413cf9ba210
Alisa <korneevaalisa11@gmail.com>
2025-11-08 19:40:16 +0300

    Catalog

--- app/src/main/java/com/example/myapplicationshop/SecondActivity.kt
+++ app/src/main/java/com/example/myapplicationshop/SecondActivity.kt
@@ -3,18 +3,34 @@ package com.example.myapplicationshop
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import Product
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class SecondActivity : AppCompatActivity() {
+
+       private val products = listOf(
+           Product( 1, "Cospley1",   18000.50,    "Описание 1",   R.drawable.one),
+           Product(2, "Cospley2",   16000.50,  "Описание 2",   R.drawable.two),
+           Product(3, "Cospley3",   15000.50,   "Описание 3",   R.drawable.three),
+           Product(4,  "Cospley4",   30000.50,  "Описание 4",   R.drawable.four),
+       )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.second)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val container = findViewById<LinearLayout>(R.id.catalog)
+
+        products.forEach { product ->
+            val view = layoutInflater.inflate(R.layout.Item_product, container, false)
+
+            view.findViewById<ImageView>(R.id.ivProductImage).setImageResource(product.ImageRes)
+            view.findViewById<TextView>(R.id.tvProductname).text = product.name
+            view.findViewById<TextView>(R.id.tvProductPrice).text = "${product.prise} ₽"
+            container.addView(view )
         }
     }
 }
---
+++ app/src/main/java/com/example/myapplicationshop/model/Product.kt
@@ -0,0 +1,9 @@
+
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val prise: Double,
+    val descriptor: String = "",
+    val ImageRes: Int
+)
